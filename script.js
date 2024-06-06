b13ae59bd027ec240a5804e65f311ab5e6befa4a
devmohamoud <devmohamoud@gmail.com>
2024-06-06 14:39:51 -0700

    create a fn to prompt person player to chose between rock, paper, scissors

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-
+const rl = require("readline").createInterface({ input: process.stdin, output: process.stdout });
 
 function getComputerChoice(arr) {
     const randonChoice = Math.floor(Math.random() * arr.length);
     return arr[randonChoice];
 }
-const arr = ["rock", "paper", "scissors"];
-console.log(getComputerChoice(arr))
-console.log(getComputerChoice(arr))
-console.log(getComputerChoice(arr))
-console.log(getComputerChoice(arr))
+
+
+function getPersonChoice() {
+	rl.question("Enter 1 choice from the list 'rock', 'paper', 'scissors': ", (choice) => {
+		if (choice === 'rock') {
+			console.log(`You chose: ${choice}`);
+		}
+		if (choice === 'paper') {
+			console.log(`You chose: ${choice}`);
+		} 
+		if (choice === 'scissors') {
+			console.log(`You chose: ${choice}`);
+
+		}
+		rl.close();
+	});
+};
+console.log(getPersonChoice());
