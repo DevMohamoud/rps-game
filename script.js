ceae0d1e4793804d0d29fbfc85846859629cdbe8
devmohamoud <devmohamoud@gmail.com>
2024-06-11 16:00:20 -0700

    Get human input, computer input, keep up with both players score, generate structure to play one round, then add more logic to play full game

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 const rl = require("readline").createInterface({ input: process.stdin, output: process.stdout });
 
-function getComputerChoice(arr) {
-    const randonChoice = Math.floor(Math.random() * arr.length);
-    return arr[randonChoice];
+function getComputerChoice() {
+  const arr = ['rock', 'paper', 'scissors'];
+  let getChoice = Math.floor(Math.random() * arr.length);
+  return arr[getChoice];   
+}
+
+function getHumanChoice() {
+  let getChoice = prompt("Play rock, paper or scissors ");
+  return getChoice.toLowerCase();
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = getHumanChoice();
+  computerChoice = getComputerChoice();
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log(`You won ${humanChoice} beats ${computerChoice}🔥`);
+    humanScore++;
+    console.log(`Human score: ${humanScore}`);
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log(`You won ${humanChoice} beats ${computerChoice}🔥`);
+     humanScore++;
+     console.log(`Human score: ${humanScore}`);
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log(`You won ${humanChoice} beats ${computerChoice}🔥`);
+     humanScore++;
+     console.log(`Human score: ${humanScore}`);
+  } else {
+    console.log(`You lose ${computerChoice} beats ${humanChoice}🤖`);
+    computerScore++;
+    console.log(`computer score: ${computerScore}`);
+  }
+}
+
+function playGame() {
+  for (let i=0; i<5; i++) {
+    playRound();
+  }
 }
 
 
-function getPersonChoice() {
-	rl.question("Enter 1 choice from the list 'rock', 'paper', 'scissors': ", (choice) => {
-		if (choice === 'rock') {
-			console.log(`You chose: ${choice}`);
-		}
-		if (choice === 'paper') {
-			console.log(`You chose: ${choice}`);
-		} 
-		if (choice === 'scissors') {
-			console.log(`You chose: ${choice}`);
 
-		}
-		rl.close();
-	});
-};
-console.log(getPersonChoice());
+
